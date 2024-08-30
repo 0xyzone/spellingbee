1676ebff3c3f34be9097cb53d7d70e3a86f10f74
0xyzone <58982560+0xyzone@users.noreply.github.com>
2024-08-30 20:41:51 +0545

    Update guest.blade.php

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -19,10 +19,12 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.15.2/js/selectize.min.js"
     integrity="sha512-IOebNkvA/HZjMM7MxL0NYeLYEalloZ8ckak+NDtOViP7oiYzG5vn6WVXyrJDiJPhl4yRdmNAG49iuLmhkUdVsQ=="
     crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     
     <!-- Scripts -->
     <script async src="https://www.google.com/recaptcha/api.js">
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    
 </head>
 
 <body class="font-sans text-gray-900 antialiased">

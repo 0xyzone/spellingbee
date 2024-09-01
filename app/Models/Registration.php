a2786cda365e0f6ca51dbd79f06d6be1b70da2d4
0xyzone <58982560+0xyzone@users.noreply.github.com>
2024-09-01 19:03:22 +0545

    Update Registration.php

--- app/Models/Registration.php
+++ app/Models/Registration.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
+use App\Models\Event;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Registration extends Model
 {

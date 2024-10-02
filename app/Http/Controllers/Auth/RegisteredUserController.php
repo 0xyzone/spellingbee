67c58faf148f58814479181ade17d6c3b6c6defb
0xyzone <58982560+0xyzone@users.noreply.github.com>
2024-10-02 17:36:01 +0545

    updated registration logic

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -33,48 +33,69 @@ class RegisteredUserController extends Controller
      */
     public function store(Request $request): RedirectResponse
     {
-        $recaptcha_response = $request->input('g-recaptcha-response');
+        // $recaptcha_response = $request->input('g-recaptcha-response');
 
-        if (is_null($recaptcha_response)) {
-            return redirect()->back()->withInput($request->input())->with('status', 'Please Complete the Recaptcha to proceed');
-        }
+        // if (is_null($recaptcha_response)) {
+        //     return redirect()->back()->withInput($request->input())->with('status', 'Please Complete the Recaptcha to proceed');
+        // }
 
-        $url = "https://www.google.com/recaptcha/api/siteverify";
+        // $url = "https://www.google.com/recaptcha/api/siteverify";
 
-        $body = [
-            'secret' => config('services.recaptcha.secret'),
-            'response' => $recaptcha_response,
-            'remoteip' => IpUtils::anonymize($request->ip()) //anonymize the ip to be GDPR compliant. Otherwise just pass the default ip address
-        ];
+        // $body = [
+        //     'secret' => config('services.recaptcha.secret'),
+        //     'response' => $recaptcha_response,
+        //     'remoteip' => IpUtils::anonymize($request->ip()) //anonymize the ip to be GDPR compliant. Otherwise just pass the default ip address
+        // ];
 
-        $response = Http::asForm()->post($url, $body);
+        // $response = Http::asForm()->post($url, $body);
 
-        $result = json_decode($response);
+        // $result = json_decode($response);
 
-        if ($response->successful() && $result->success == true) {
-            $request->validate([
-                'name' => ['required', 'string', 'max:255'],
-                'username' => ['required', 'string', 'max:255', Rule::unique('users')],
-                'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
-                'password' => ['required', 'confirmed', Rules\Password::defaults()],
-            ]);
+        // if ($response->successful() && $result->success == true) {
+        //     $request->validate([
+        //         'name' => ['required', 'string', 'max:255'],
+        //         'username' => ['required', 'string', 'max:255', Rule::unique('users')],
+        //         'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
+        //         'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        //     ]);
 
-            $user = User::create([
-                'name' => $request->name,
-                'username' => $request->name,
-                'email' => $request->email,
-                'password' => Hash::make($request->password),
-            ]);
+        //     $user = User::create([
+        //         'name' => $request->name,
+        //         'username' => $request->name,
+        //         'email' => $request->email,
+        //         'password' => Hash::make($request->password),
+        //     ]);
 
-            $user->assignRole('Participant');
+        //     $user->assignRole('Participant');
 
-            event(new Registered($user));
+        //     event(new Registered($user));
 
-            Auth::login($user);
+        //     Auth::login($user);
 
-            return redirect(RouteServiceProvider::HOME);
-        } else {
-            return redirect()->back()->withInput()->with('status', 'Please Complete the Recaptcha Again to proceed');
-        }
+        //     return redirect(RouteServiceProvider::HOME);
+        // } else {
+        //     return redirect()->back()->withInput()->with('status', 'Please Complete the Recaptcha Again to proceed');
+        // }
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'username' => ['required', 'string', 'max:255', Rule::unique('users')],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $user->assignRole('Participant');
+
+        event(new Registered($user));
+
+        Auth::login($user);
+
+        return redirect(RouteServiceProvider::HOME);
     }
 }

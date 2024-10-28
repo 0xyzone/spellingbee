819b0eb709eb00a2c2c996e9801225a374ac8514
0xyzone <58982560+0xyzone@users.noreply.github.com>
2024-10-28 06:08:22 +0545

    Update footer.blade.php

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -3,14 +3,14 @@
         <div class="border-2 border-x-0 border-t-0 pb-5 border-current">
             <h1 class="text-xl font-extrabold">Connect With Us</h1>
             <div class="mt-5 flex flex-col font-semibold gap-3">
-                <div class="flex items-center gap-2">
+                <a href="https://maps.app.goo.gl/5he7Mca3sqxijcK46" class="flex items-center gap-2">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                         <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
                     </svg>
 
                     <h4>Phusinghkhya (Manamaiju) Tarakeshwor Municipality</h4>
-                </div>
+                </a>
                 <div class="flex items-center gap-2">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />

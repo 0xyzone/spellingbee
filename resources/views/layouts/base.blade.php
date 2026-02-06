efcf1a7013ffb7c116dd7bff4a8257dbc4fe7347
0xyzone <58982560+0xyzone@users.noreply.github.com>
2026-02-06 15:35:37 +0545

    Update base.blade.php

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="shortcut icon" href="{{ asset('images/sbn2024.png') }}" type="image/x-icon">
     <title>Spelling Bee Nepal 2026</title>
 
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />

a1a5d96e9f67ed047b29548cfa9ef5ff03c35486
0xyzone <58982560+0xyzone@users.noreply.github.com>
2026-02-06 18:29:05 +0545

    Update Sponsor.php

--- app/Models/Sponsor.php
+++ app/Models/Sponsor.php
@@ -9,10 +9,22 @@ class Sponsor extends Model
 {
     use HasFactory;
 
+    protected $appends = ['sponsor_logo_url'];
     protected $fillable = [
         "name",
-        "link",
+        "url",
         "description",
-        "logo",
+        "sponsor_logo_path",
     ] ;
+
+    /**
+     * Create the sponsor_logo_url attribute
+     */
+    public function getSponsorLogoUrlAttribute() 
+    {
+        // Use the actual column name from your database
+        return $this->sponsor_logo_path 
+            ? asset('storage/' . $this->sponsor_logo_path) 
+            : null;
+    }
 }

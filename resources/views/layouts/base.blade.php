3c41ab6335cbac1e2ca002593e2d49f98951b10a
0xyzone <58982560+0xyzone@users.noreply.github.com>
2026-02-10 22:48:09 +0545

    added

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="shortcut icon" href="{{ asset('images/sbn2024.png') }}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
     <title>Spelling Bee Nepal</title>
 
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
